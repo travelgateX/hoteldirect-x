964df8e601f18d0d316c609404f3d173b9dfdd7f
Cristian Lladó <cllado@xmltravelgate.com>
2019-08-08 16:38:47 +0200

    added hoteldirect-x files

---
+++ hoteldirectx/src/app/components/hoteldirectx-home/hoteldirectx-home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hoteldirectx-home',
+  templateUrl: './hoteldirectx-home.component.html',
+  styleUrls: ['./hoteldirectx-home.component.scss']
+})
+export class HoteldirectxHomeComponent implements OnInit {
+
+  letsee: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
